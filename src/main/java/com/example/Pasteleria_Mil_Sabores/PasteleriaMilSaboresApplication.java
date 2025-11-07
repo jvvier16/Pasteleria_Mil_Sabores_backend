ca34d2c55b305ba897eed8605d36286da8941832
PAAWSA02LC1001 <Duoc@PAAWSA02LC1001.duoc.acad>
2025-11-07 12:04:53 -0300

    primer commit

---
+++ src/main/java/com/example/Pasteleria_Mil_Sabores/PasteleriaMilSaboresApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pasteleria_Mil_Sabores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteleriaMilSaboresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteleriaMilSaboresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pasteleria_Mil_Sabores/PasteleriaMilSaboresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pasteleria_Mil_Sabores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteleriaMilSaboresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
