d5bca4fdf0c6ef0c17f9bb1312cfa2b9ade6ab82
araZ <ara.zenteno@duocuc.cl>
2025-11-23 00:11:06 -0300

    Controller y Service
    - Se crearon todos los Services y Controller
    - Se modificó el ProductoRepository para permitir la busqueda por palabra clave

--- src/main/java/com/example/Pasteleria_Mil_Sabores/Repository/ProductoRepository.java
+++ src/main/java/com/example/Pasteleria_Mil_Sabores/Repository/ProductoRepository.java
@@ -3,8 +3,10 @@ package com.example.Pasteleria_Mil_Sabores.Repository;
 import com.example.Pasteleria_Mil_Sabores.Entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    // e.g. find by nombre if needed: List<Producto> findByNombreContainingIgnoreCase(String nombre);
+    // Para buscar productos cuyo nombre contiene la palabra clave (ignora mayúsculas/minúsculas)
+    List<Producto> findByNombreContainingIgnoreCase(String nombre);
 }
