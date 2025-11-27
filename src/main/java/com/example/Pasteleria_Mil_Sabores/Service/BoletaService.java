b65f47ca3af52cdd0327fdd7a69308f8e23cbe20
araZ <ara.zenteno@duocuc.cl>
2025-11-27 19:30:02 -0300

    Revert "Conexion Backend/Wallet"
    This reverts commit adaedb9422ea390157f342d36d90ee2f73152270.

--- src/main/java/com/example/Pasteleria_Mil_Sabores/Service/BoletaService.java
+++ src/main/java/com/example/Pasteleria_Mil_Sabores/Service/BoletaService.java
@@ -3,7 +3,6 @@ package com.example.Pasteleria_Mil_Sabores.Service;
 import com.example.Pasteleria_Mil_Sabores.Entity.Boleta;
 import com.example.Pasteleria_Mil_Sabores.Repository.BoletaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -14,8 +13,7 @@ public class BoletaService {
     private BoletaRepository boletaRepository;
 
     // POST: Crear/Guardar una boleta (Esto representa la finalización de una compra)
-    @NonNull
-    public Boleta crearBoleta(@NonNull Boleta boleta) {
+    public Boleta crearBoleta(Boleta boleta) {
         // Lógica clave: Calcular el total, guardar detalles de boleta, integrar pago (sandbox),
         // y descontar stock de los productos.
         return boletaRepository.save(boleta);
@@ -27,7 +25,7 @@ public class BoletaService {
     }
 
     // GET BY ID: Obtener una boleta por su ID
-    public Boleta obtenerBoletaPorId(@NonNull Long id) {
+    public Boleta obtenerBoletaPorId(Long id) {
         return boletaRepository.findById(id).orElse(null);
     }
     
